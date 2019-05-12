3f47964f5f69da04e094f1a6a991654fe715e9b2
Geo Jimenez <ger.jimenez1@gmail.com>
2019-05-12 12:03:07 -0400

    initial commit

---
+++ app/src/main/java/org/pursuit/usolo/HostActivity.java
@@ -0,0 +1,13 @@
+package org.pursuit.usolo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class HostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+    }
+}
