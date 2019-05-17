1c35c20a276346f839231f12f60815beb94d8586
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-05-16 21:41:09 -0400

    Implementing expandable Fab button

--- app/src/main/java/org/pursuit/usolo/map/MapFragment.java
+++ app/src/main/java/org/pursuit/usolo/map/MapFragment.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.mapbox.android.telemetry.MapboxTelemetry;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -23,8 +27,11 @@ import com.mapbox.mapboxsdk.maps.Style;
 
 import org.pursuit.usolo.R;
 
-public final class MapFragment extends Fragment {
+public final class MapFragment extends Fragment implements View.OnClickListener {
     private MapView mapView;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab1, fab2;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -50,15 +57,37 @@ public final class MapFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        View bottomSheet = view.findViewById( R.id.bottom_sheet );
+        findViews(view);
+        loadFabAnimations();
+        setOnClicks();
+        View bottomSheet = view.findViewById(R.id.bottom_sheet);
         BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         bottomSheetBehavior.setPeekHeight(130);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         mapView = view.findViewById(R.id.mapView);
         mapView.getMapAsync(mapboxMap ->
-                mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/naomyp/cjvpowkpn0yd01co7844p4m6w"), style -> {
-                    // TODO: Map is set up and the style has loaded. Now you can add data or make other map adjustments
-                }));
+          mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/naomyp/cjvpowkpn0yd01co7844p4m6w"), style -> {
+              // TODO: Map is set up and the style has loaded. Now you can add data or make other map adjustments
+          }));
+    }
+
+    private void setOnClicks() {
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+    }
+
+    private void loadFabAnimations() {
+        fab_open = AnimationUtils.loadAnimation(getContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getContext(), R.anim.fab_rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getContext(), R.anim.fab_rotate_backward);
+    }
+
+    private void findViews(@NonNull View view) {
+        fab = view.findViewById(R.id.fab);
+        fab1 = view.findViewById(R.id.fab1);
+        fab2 = view.findViewById(R.id.fab2);
     }
 
     @Override
@@ -95,5 +124,37 @@ public final class MapFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         mapView.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.fab:
+                animateFAB();
+                break;
+            case R.id.fab1:
+                break;
+            case R.id.fab2:
+                break;
+        }
+    }
+
+    public void animateFAB() {
+        if (isFabOpen) {
+            fab.startAnimation(rotate_backward);
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 }
