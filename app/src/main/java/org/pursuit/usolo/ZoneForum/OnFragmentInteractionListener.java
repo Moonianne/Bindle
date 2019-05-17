a837550b0e0f3fab4c30d050dffa2c51d6f98adc
rosannaNaomy <=>
2019-05-17 18:01:53 -0400

    zone marker method in mapFragment, forumselectionFragment added

---
+++ app/src/main/java/org/pursuit/usolo/ZoneForum/OnFragmentInteractionListener.java
@@ -0,0 +1,7 @@
+package org.pursuit.usolo.ZoneForum;
+
+import android.net.Uri;
+
+public interface OnFragmentInteractionListener {
+    void onFragmentInteraction(Uri uri);
+}
--- app/src/main/java/org/pursuit/usolo/map/MapFragment.java
+++ app/src/main/java/org/pursuit/usolo/map/MapFragment.java
@@ -2,6 +2,7 @@ package org.pursuit.usolo.map;
 
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -23,7 +25,13 @@ import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.annotation.CircleManager;
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
 
 import org.pursuit.usolo.HostActivity;
 import org.pursuit.usolo.R;
@@ -39,13 +47,18 @@ public final class MapFragment extends Fragment
     private static final String MAPBOX_STYLE_URL =
       "mapbox://styles/naomyp/cjvpowkpn0yd01co7844p4m6w";
 
+    private static final String ID_ICON_DEFAULT = "icon-default";
+    private static final String MARKER_IMAGE = "custom-marker";
+
     private MapView mapView;
     private boolean isFabOpen;
     private FloatingActionButton fab, fab1, fab2;
     private BottomSheetBehavior bottomSheetBehavior;
     private Animation fabOpen, fabClose, rotateForward, rotateBackward;
     private MapboxMap mapboxMap;
-
+    private SymbolManager symbolManager;
+    private Symbol symbol;
+    private CircleManager circleManager;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -112,10 +125,57 @@ public final class MapFragment extends Fragment
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         mapView = view.findViewById(R.id.mapView);
         mapView.getMapAsync(mapboxMap -> {
-            this.mapboxMap = mapboxMap;
-            // TODO: Map is set up and the style has loaded. Now you can add data or make other map adjustments
-            mapboxMap.setStyle(new Style.Builder().fromUrl(MAPBOX_STYLE_URL), this::enableLocationComponent);
+            MapFragment.this.mapboxMap = mapboxMap;
+            mapboxMap.setStyle(new Style.Builder().fromUrl(MAPBOX_STYLE_URL), new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+
+                    LatLng pursuit = new LatLng(40.7430877, -73.9419287);
+                    addZoneMarker(style, pursuit);
+
+                }
+            });
         });
+    }
+
+    private void addZoneMarker(Style style, LatLng zone) {
+
+        style.addImage(MARKER_IMAGE, BitmapFactory.decodeResource(
+          MapFragment.this.getResources(), R.drawable.asset_icon));
+
+        // create symbol manager
+        GeoJsonOptions geoJsonOptions = new GeoJsonOptions().withTolerance(0.4f);
+        symbolManager = new SymbolManager(mapView, mapboxMap, style, null, geoJsonOptions);
+        symbolManager.addClickListener(symbol -> Toast.makeText(getContext(),
+          String.format("Symbol clicked %s", symbol.getId()), Toast.LENGTH_SHORT).show());
+        symbolManager.addLongClickListener(symbol ->
+          Toast.makeText(getContext(),
+            String.format("Symbol long clicked %s", symbol.getId()), Toast.LENGTH_SHORT).show());
+
+        // set non data driven properties
+        symbolManager.setIconAllowOverlap(true);
+        symbolManager.setTextAllowOverlap(true);
+
+        // create a symbol
+        SymbolOptions symbolOptions = new SymbolOptions()
+          .withLatLng(zone) //replace with zone LatLng
+          .withIconImage(MARKER_IMAGE)
+          .withIconSize(.5f);
+        symbol = symbolManager.create(symbolOptions);
+
+//        // create circle manager
+//        circleManager = new CircleManager(mapView, mapboxMap, style);
+//        circleManager.addClickListener(circle -> Toast.makeText(getContext(), String.format("Circle clicked %s", circle.getId()), Toast.LENGTH_SHORT).show());
+//        circleManager.addLongClickListener(circle -> Toast.makeText(getContext(), String.format("Circle long clicked %s", circle.getId()), Toast.LENGTH_SHORT).show());
+//
+//        // create a fixed circle
+//        CircleOptions circleOptions = new CircleOptions()
+//          .withLatLng(zone)
+//          .withCircleColor(ColorUtils.colorToRgbaString(Color.LTGRAY))
+//          .withCircleRadius(20f);
+//        circleManager.create(circleOptions);
+
     }
 
     private void makeGeoFence(LatLng latLng) {
