f16e76c9126f8181f9d7719cc5ad78f6be49ce46
Geo Jimenez <ger.jimenez1@gmail.com>
2019-05-18 11:45:49 -0400

    create chat room back-view-backbone

---
+++ zonechat/src/main/java/org/pursuit/zonechat/model/Message.java
@@ -0,0 +1,7 @@
+package org.pursuit.zonechat.model;
+
+public final class Message {
+    String name;
+    String message;
+    long timeStamp;
+}
---
+++ zonechat/src/main/java/org/pursuit/zonechat/model/UserState.java
@@ -0,0 +1,25 @@
+package org.pursuit.zonechat.model;
+
+public final class UserState {
+    private String group;
+    private String zone;
+
+    public UserState() {
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+}
