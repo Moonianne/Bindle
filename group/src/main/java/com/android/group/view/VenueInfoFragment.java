285af15a6eef3df14b4a9607dec25ab375e6a745
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-05-23 14:05:47 -0400

    Setting text with venue name and address

--- group/src/main/java/com/android/group/view/VenueInfoFragment.java
+++ group/src/main/java/com/android/group/view/VenueInfoFragment.java
@@ -2,12 +2,15 @@ package com.android.group.view;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.group.R;
 import com.android.group.model.Venue;
@@ -18,6 +21,9 @@ import java.util.List;
 public class VenueInfoFragment extends Fragment {
     private String venueName;
     private ArrayList<String> venueAddress;
+    private ImageView imageViewVenue;
+    private TextView textViewVenueName;
+    private TextView textViewVenueAddress;
 
     public VenueInfoFragment() {
     }
@@ -44,5 +50,19 @@ public class VenueInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_venue_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findViews(view);
+        textViewVenueName.setText(venueName);
+        textViewVenueAddress.setText(venueAddress.get(0));
+    }
+
+    private void findViews(@NonNull View view) {
+        imageViewVenue = view.findViewById(R.id.imageView_venue_picture);
+        textViewVenueName = view.findViewById(R.id.textView_venue_name);
+        textViewVenueAddress = view.findViewById(R.id.textView_venue_address);
     }
 }
