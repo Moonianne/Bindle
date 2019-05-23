692b8b5910703be2deed5cb08479430df9a2a409
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-05-22 22:39:11 -0400

    Adding inflateVenueFragment method in OnFragmentInteractionListener and defining in HostActivity

--- interactionlistener/src/main/java/com/android/interactionlistener/OnFragmentInteractionListener.java
+++ interactionlistener/src/main/java/com/android/interactionlistener/OnFragmentInteractionListener.java
@@ -4,6 +4,7 @@ package com.android.interactionlistener;
 public interface OnFragmentInteractionListener {
 
     void inflateStartGroupFragment();
-
     void inflateZoneChatFragment();
+    void inflateVenueInfoFragment();
+
 }
