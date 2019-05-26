1e2ad3558eef09ee0a173c3d899b126f645f4f11
Joseph Salcedo <joseph.salcedo16@qmail.cuny.edu>
2019-05-25 22:00:58 -0400

    Creating Yelp model classes

---
+++ group/src/main/java/com/android/group/model/yelp/YelpResponse.java
@@ -0,0 +1,11 @@
+package com.android.group.model.yelp;
+
+import java.util.List;
+
+public class YelpResponse {
+    List<Business> businesses;
+
+    public List<Business> getBusinesses() {
+        return businesses;
+    }
+}
