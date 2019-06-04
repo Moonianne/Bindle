549e3ea2994904f326c1453954cbf5de1645e129
Eric Diaz <ericdiaz@pursuit.org>
2019-06-03 20:54:03 -0400

    itemView version 1 complete

--- exploregroup/src/main/java/com/example/exploregroup/view/ViewGroupFragment.java
+++ exploregroup/src/main/java/com/example/exploregroup/view/ViewGroupFragment.java
@@ -28,7 +28,6 @@ public class ViewGroupFragment extends Fragment {
     public static final String GROUP_PREFS = "GROUP";
     public static final String CURRENT_GROUP_KEY = "current_group";
     private OnFragmentInteractionListener listener;
-    private FireRepo fireRepo;
     private Group group;
     private SharedPreferences sharedPrefs;
 
@@ -58,13 +57,22 @@ public class ViewGroupFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_view_group, container, false);
+        setViews(rootView);
+        initJoinGroupButtonListener(rootView);
+        return rootView;
+    }
+
+    private void setViews(View rootView) {
         if (getArguments() != null) {
             group = (Group) getArguments().getSerializable(GROUP_KEY);
             Picasso.get().load(group.getImage_url()).into(rootView.<ImageView>findViewById(R.id.view_group_location_image_view));
             rootView.<TextView>findViewById(R.id.view_group_name_text_view).setText(group.getTitle());
+            rootView.<TextView>findViewById(R.id.view_group_address).setText(group.getAddress());
             rootView.<TextView>findViewById(R.id.view_group_description_text_view).setText(group.getDescription());
         }
+    }
 
+    private void initJoinGroupButtonListener(View rootView) {
         rootView.findViewById(R.id.join_group_button).setOnClickListener(v -> {
             if (!sharedPrefs.contains(CURRENT_GROUP_KEY)) {
                 sharedPrefs.edit().putString(CURRENT_GROUP_KEY, group.getTitle()).apply();
@@ -72,10 +80,7 @@ public class ViewGroupFragment extends Fragment {
             } else {
                 Toast.makeText(getContext(), "Already in Group", Toast.LENGTH_SHORT).show();
             }
-
         });
-        fireRepo = FireRepo.getInstance();
-        return rootView;
     }
 
 }
