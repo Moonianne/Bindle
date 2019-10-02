23ea978835450b13b0585d99cf4b73e4ea32685d
Eric Diaz <ericdiaz@pursuit.org>
2019-10-02 18:20:48 -0400

    update notifications to work on API 25 and lower

--- app/src/main/java/org/pursuit/usolo/map/utils/Notification.java
+++ app/src/main/java/org/pursuit/usolo/map/utils/Notification.java
@@ -7,12 +7,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import org.pursuit.usolo.R;
 import org.pursuit.usolo.view.HostActivity;
 
 public class Notification {
 
+    private static final String TAG = "Notification";
     private static final String CHANNEL_ID = "testMap";
     private static final int NOTIFICATION_ID = 2;
     private Context context;
@@ -36,24 +38,34 @@ public class Notification {
         return builder.build();
     }
 
-    private PendingIntent getPendingIntent(){
-        return PendingIntent.getActivity(context,3,
-          new Intent(context, HostActivity.class),PendingIntent.FLAG_UPDATE_CURRENT);
+    private PendingIntent getPendingIntent() {
+        return PendingIntent.getActivity(context, 3,
+          new Intent(context, HostActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Map Notification";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        CharSequence name = "Map Notification";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+        } else {
+            notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(
+              new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT));
         }
     }
 
     private void sendNotification() {
-        notificationManager.notify(NOTIFICATION_ID, buildNotification());
-        context = null;
+        if (notificationManager != null) {
+            notificationManager.notify(NOTIFICATION_ID, buildNotification());
+            context = null;
+        } else {
+            Log.d(TAG, "could not send notification " + message + " because" +
+              "of null notification manager");
+        }
     }
 
 }
